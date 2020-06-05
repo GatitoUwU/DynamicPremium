e0bd89333e31074131d2ea790a4d1f3ca7b2dc98
Internal exception: java.lang.nullPointerException <dilanolivera12@gmail.com>
2020-06-05 16:27:32 -0300

    Fixed configurations
    Fixed a bug with configurations (I back to the old system)

--- src/main/java/me/gatogamer/dynamicpremium/bungee/config/ConfigCreator.java
+++ src/main/java/me/gatogamer/dynamicpremium/bungee/config/ConfigCreator.java
@@ -2,25 +2,72 @@ package me.gatogamer.dynamicpremium.bungee.config;
 
 import net.md_5.bungee.api.plugin.Plugin;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
+import com.google.common.io.ByteStreams;
+import net.md_5.bungee.api.plugin.Plugin;
+
+import java.io.*;
+
 
 public class ConfigCreator {
     private static ConfigCreator instance;
 
-    public void setupBungee(Plugin p, String file) {
-        try {
-            File configFile = new File(p.getDataFolder(), file+".yml");
-            if (!configFile.exists()) {
-                final InputStream is = p.getResourceAsStream(file + ".yml");
-                if (is != null) {
-                    Files.copy(is, configFile.toPath());
+    public void setupBungee(final Plugin p, final String configname) {
+        if (!p.getDataFolder().exists()) {
+            p.getDataFolder().mkdir();
+        }
+        final File configFile = new File(p.getDataFolder(), String.valueOf(configname) + ".yml");
+        if (!configFile.exists()) {
+            try {
+                configFile.createNewFile();
+                Throwable t = null;
+                try {
+                    final InputStream is = p.getResourceAsStream(String.valueOf(configname) + ".yml");
+                    try {
+                        final OutputStream os = new FileOutputStream(configFile);
+                        try {
+                            ByteStreams.copy(is, os);
+                        }
+                        finally {
+                            if (os != null) {
+                                os.close();
+                            }
+                        }
+                        if (is != null) {
+                            is.close();
+                        }
+                    }
+                    finally {
+                        if (t == null) {
+                            final Throwable t2 = null;
+                            t = t2;
+                        }
+                        else {
+                            final Throwable t2 = null;
+                            if (t != t2) {
+                                t.addSuppressed(t2);
+                            }
+                        }
+                        if (is != null) {
+                            is.close();
+                        }
+                    }
+                }
+                finally {
+                    if (t == null) {
+                        final Throwable t3 = null;
+                        t = t3;
+                    }
+                    else {
+                        final Throwable t3 = null;
+                        if (t != t3) {
+                            t.addSuppressed(t3);
+                        }
+                    }
                 }
             }
-        } catch (IOException ignored) {
-            System.out.println("Error on create " + file + " please contact with Developer: gatogamer#1111");
+            catch (IOException e) {
+                throw new RuntimeException("Error on create " + configname + " please contact with Developer: gatogamer#1111", e);
+            }
         }
     }
 
